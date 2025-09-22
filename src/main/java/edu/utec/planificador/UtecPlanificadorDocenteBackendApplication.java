49606238d2e0943008ace4627dfe3cb5d822ad6e
Valentín Veintemilla <valentinveintemilla@gmail.com>
2025-09-22 00:49:24 -0300

    Initialize Spring Boot project with Gradle setup and configuration files

---
+++ src/main/java/edu/utec/planificador/UtecPlanificadorDocenteBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.utec.planificador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtecPlanificadorDocenteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtecPlanificadorDocenteBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/utec/planificador/UtecPlanificadorDocenteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.utec.planificador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtecPlanificadorDocenteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
