a86e9abaa962f487e969b1fd7bf822a8dc05e07a
Valentín Veintemilla Marchisio <valentinveintemilla@gmail.com>
2025-10-28 02:15:12 -0300

    Add JPA config and entities scanning annotations

--- src/main/java/edu/utec/planificador/UtecPlanificadorDocenteBackendApplication.java
+++ src/main/java/edu/utec/planificador/UtecPlanificadorDocenteBackendApplication.java
@@ -2,12 +2,16 @@ package edu.utec.planificador;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("edu.utec.planificador.entity")
+@EnableJpaRepositories("edu.utec.planificador.repository")
 public class UtecPlanificadorDocenteBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UtecPlanificadorDocenteBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UtecPlanificadorDocenteBackendApplication.class, args);
+    }
 
 }
